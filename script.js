154e8ee4f67c1055020537b8345907059a044a25
jamie-boeing <bleupaon@outlook.com>
2023-04-30 09:09:05 -0500

    adding timer function with bonus points

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ let achievements = []
 //define questions array
 let questionData = []
 
+//set up timer variables
+let startTime
+let endTime
+let timer
+
 // question data from the JSON file
 function loadQuestionData() {
     fetch('questions.json')
@@ -23,7 +28,9 @@ function loadQuestionData() {
 //load to next question
 function loadQuestion() {
     //check if all questions in the current level have been answered
-    if(currentLevel >= questionData.length) {
+    if (currentQuestion >= question.Data[currentLevel - 1].questions.length) {
+        //move to the next leve or end the game if all levels have been completed
+    if(currentLevel < questionData.length) {
         currentLevel++
         currentQuestion = 0
         levelBonus += 10 //add points for level up
@@ -33,3 +40,27 @@ function loadQuestion() {
         return
     }
 }
+ const question = questionData[currentLevel - 1].questions[currentQuestion]
+ // load the question and answer options
+
+ // start the timer
+ startTime = new Date().getTime()
+ timer = setTimeout(() => {
+    endTimer()
+ }, 10000) // 10 seconds time limit
+}
+
+// end the timer and add bonus points
+function endTimer() {
+    endTime = new Date().getTime()
+    const timeElapsed = endTime - startTime
+
+    if(timeElapsed <= 10000) {//time limit in milliseconds
+        timeBonus = 5 //set the bonus points
+    } else {
+        timeBonus = 0 //no bonus if exceeded time limit
+    }
+    currentQuestion++
+    loadQuestion()
+}
+// function to handle answering a question 
