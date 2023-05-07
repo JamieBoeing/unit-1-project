c6b3c62d8188a0cfec6713ea89c29d143907428b
jamie-boeing <bleupaon@outlook.com>
2023-05-06 20:34:18 -0500

    displayquestion()

--- script.js
+++ script.js
@@ -1,76 +1,85 @@
 // Set up variables for tracking game state
 let currentLevel = 1
-let currentQuestion =0
+let currentQuestion = 0
 let score = 0
-let timer = 0
 let questionData = []
+let questionTimer = null
+let timeLeft = 0
+let timerEl = document.querySelector("#timer") //select timer element
+
+function startTimer() {
+   // clear any existing timer intervals
+   clearInterval(questionTimer)
+
+//set the time to the initial value
+timeLeft = 120
+
+   // set interval to count down the question time
+   questionTimer = setInterval(() => {
+    timeLeft--
+    timerEl.innerHTML = `Time remaining: ${timeLeft} seconds`
+    if (timeLeft === 0) {
+      clearInterval()
+      answerQuestion(null, timeLeft) // call answer question with a null choice
+    }
+   }, 1000)
+}
 
 
 async function loadQuestionData() {
   try {
-    const res = await fetch("questions.json")
-    const data = await res.json()
-    return data
-    } catch (err) {
-        console.error(err)
-    }
+  const response = await fetch("questions.json")
+  questionData = await response.json() 
+  displayQuestion()
+} catch (error) {
+  console.error(error)
   }
+}
 
+// function to display the current question
+function displayQuestion() {
+  const question = questionData[currentLevel - 1].questions[currentQuestion]
+  const questionEl = document.querySelector("#question")
+  questionEl.innerHTML = question.textContent
 
-// Add event listners to answer buttons
-const answerBtns = document.querySelectorAll("#game-screen btn")
-answerBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    answerQuestion(button.textContent)
-      })
-    })
+  const answerAE= document.querySelector("#answer-a")
+  answerAE.innerHTML = question.choices.a
+  answerAE.dataset.choice = "a"
+
+  const answerBE= document.querySelector("#answer-b")
+  answerBE.innerHTML = question.choices.b
+  answerBE.dataset.choice = "b"
+
+  const answerCE= document.querySelector("#answer-c")
+  answerCE.innerHTML = question.choices.c
+  answerCE.dataset.choice = "c"
+
+  const answerDE= document.querySelector("#answer-d")
+  answerDE.innerHTML = question.choices.d
+  answerDE.dataset.choice = "d"
+  startTimer() // start the timer when the question is displayed
+
+}
+
 
 // Function to start the game
 async function startGame() {
-  const questionData = await loadQuestionData()
-  console.log(questionData)
-  
   // Hide the intro screen and show the game screen
   const introScreen = document.getElementById("intro-screen")
   const gameScreen = document.getElementById("game-screen")
   introScreen.style.display = "none"
   gameScreen.style.display = "block"
   
-  //show question
- 
-  //Start the timer for time bonus
-  startTimer()
-
-  // Add event listeners to answer buttons
-  const answerA = document.getElementById("answer-a")
-  answerA.addEventListener("click", () => answerQuestion("a"))
-
-  const answerB = document.getElementById("answer-b")
-  answerB.addEventListener("click", () => answerQuestion("b"))
-
-  const answerC = document.getElementById("answer-c")
-  answerC.addEventListener("click", () => answerQuestion("c"))
-
-  const answerD = document.getElementById("answer-d")
-  answerD.addEventListener("click", () => answerQuestion("d"))
-
-  startBtn.addEventListener("click", () => {
-    questionData()
-  })
+  
+  
+await loadQuestionData()
 }
 
-const startBtn = document.getElementById("start-btn")
-startBtn.addEventListener('click', async function() {
-  await loadQuestionData()
-  startGame()
-})
-//Function to start the timer for time bonus
-function startTimer() {
 
-}
+
 // Function to handle answering a question
 function answerQuestion(choice, timeTaken) {
-  clearInterval(timer)
+  clearInterval(questionTimer)
   const question = questionData[currentLevel - 1].questions[currentQuestion]
   const correctAnswer = question.answer
 
@@ -87,33 +96,15 @@ function answerQuestion(choice, timeTaken) {
     newQuote()
   }
 
-  // Move to the next question
+  // Move to the next question or level
   currentQuestion++
-
-  // check if we've reached the end of the questions
   if (currentQuestion >= questionData[currentLevel - 1].questions.length) {
-  // end game logic here
-  endGame()
+    if (currentLevel >= questionData.length) {
+      endGame()
+    }
 } else {
-  loadQuestionData()
+  displayQuestion()
   }
-}
-const philosophers = [
-  "The only true wisdom is in knowing you know nothing. - Socrates",
-  "Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
-  "We are what we repeatedly do. Excellence, then, is not an act, but a habit. - Aristotle",
-  "I think, therefore I am. - René Descartes",
-  "The unexamined life is not worth living. - Socrates",
-  "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
-  "The only thing necessary for the triumph of evil is for good men to do nothing. - Edmund Burke",
-  "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. - Ralph Waldo Emerson",
-  "We are all in the gutter, but some of us are looking at the stars. - Oscar Wilde",
-  "The mind is everything. What you think you become. - Buddha"
-]
-// Add messages for end game quotes
-function newQuote() {
-  const randomIndex = Math.floor(Math.random() * philosophers.length)
-  return philosophers[randomIndex]
 }
 
 // Function to end the game
@@ -132,8 +123,27 @@ function endGame() {
 
   // reload inital level and questions
   loadQuestionData()
-    displayScore()
-    displayLevel() 
+  displayScore()
+  displayLevel() 
+}
+
+// end game message array
+const philosophers = [
+  "The only true wisdom is in knowing you know nothing. - Socrates",
+  "Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
+  "We are what we repeatedly do. Excellence, then, is not an act, but a habit. - Aristotle",
+  "I think, therefore I am. - René Descartes",
+  "The unexamined life is not worth living. - Socrates",
+  "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
+  "The only thing necessary for the triumph of evil is for good men to do nothing. - Edmund Burke",
+  "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. - Ralph Waldo Emerson",
+  "We are all in the gutter, but some of us are looking at the stars. - Oscar Wilde",
+  "The mind is everything. What you think you become. - Buddha"
+]
+// Add messages for end game quotes
+function newQuote() {
+  const randomIndex = Math.floor(Math.random() * philosophers.length)
+  return philosophers[randomIndex]
 }
 
 // Function to display the current level
@@ -159,5 +169,24 @@ function playWrongSound() {
   const wrongSound = document.getElementById("wrong-sound")
   wrongSound.play()
 }
+
+
+// Add event listeners to answer buttons
+const answerA = document.getElementById("answer-a")
+answerA.addEventListener("click", () => answerQuestion("a"))
+
+const answerB = document.getElementById("answer-b")
+answerB.addEventListener("click", () => answerQuestion("b"))
+
+const answerC = document.getElementById("answer-c")
+answerC.addEventListener("click", () => answerQuestion("c"))
+
+const answerD = document.getElementById("answer-d")
+answerD.addEventListener("click", () => answerQuestion("d"))
+
+const startBtn = document.getElementById("start-btn")
+startBtn.addEventListener("click", startGame)  
+
+
 
 
